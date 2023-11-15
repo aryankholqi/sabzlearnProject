d126437e14b20a5d99868a16101f4a4ba189d338
Aryan kholqi <aryankholqi@gmail.com>
2023-11-15 23:31:30 +0330

    create routing and few pages

---
+++ src/routes.jsx
@@ -0,0 +1,13 @@
+import ArticleInfo from "./pages/ArticleInfo/ArticleInfo";
+import Category from "./pages/Category/Category";
+import CourseInfo from "./pages/CourseInfo/CourseInfo";
+import Index from "./pages/index/index";
+
+const routes = [
+  { path: "/", element: <Index /> },
+  { path: "/course-info/:courseName", element: <CourseInfo /> },
+  { path: "/category-info/:categoryName", element: <Category /> },
+  { path: "/article-info/:articleName", element: <ArticleInfo /> },
+];
+
+export default routes;
